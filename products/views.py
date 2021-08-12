30022863b95e835614685d6624ede3570c7fafa0
Vokler <valerypavlikov@yandex.ru>
2021-08-12 20:34:13 +0300

    added context in test_context() controller

--- products/views.py
+++ products/views.py
@@ -7,3 +7,17 @@ def index(request):
 
 def products(request):
     return render(request, 'products/products.html')
+
+
+def test_context(request):
+    context = {
+        'title': 'Test Context',
+        'header': 'Добро пожаловать на сайт!',
+        'username': 'Иван Иванов',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': 6090},
+            {'name': 'Синяя куртка The North Face', 'price': 23725},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': 3390},
+        ]
+    }
+    return render(request, 'products/test-context.html', context)
